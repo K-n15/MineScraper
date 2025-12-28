0fc4855e630de4ca297a4f44a9fed333c051abac
K-n15 <theepapon7055@gmail.com>
2025-12-29 02:38:58 +0700

    Managed the scraping target and filtering data

--- Seabed.py
+++ Seabed.py
@@ -1,21 +1,40 @@
-import requests
+import requests, re
+from google import genai
+from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 
-def scrape(url=''):
+def spiderQuest(url):
     headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
     response = requests.get(url,headers=headers,timeout=10)
     if response.status_code == 408 :
         return "REQUEST_TIMEOUT"
     elif  response.status_code == 504:
         return "GATEWAY_TIMEOUT"
-    soup = BeautifulSoup(response.text, 'html.parser')
-    sauce = soup.find("h1")
-    cream = soup.find("p")
-    spice = soup.find("a").get("href")
-    print(sauce.text)
-    print(cream.text)
-    print(spice)
-    print(soup.find('#start-of-content','id'))
+    return response
+
+def spider():
+    BaseUrl = 'https://en.wikinews.org'
+
+    latestNew = spiderQuest(BaseUrl+'/wiki/Main_Page')
+    soupBase = BeautifulSoup(latestNew.text,'html.parser')
+
+    FilterTitle = soupBase.find("div",{"id":"MainPage_latest_news_text","class":"latest_news_text"})
+
+    FirstNews = FilterTitle.find('a')
+    Topic = FirstNews.text
+    Link = FirstNews['href']
+
+    Detail = spiderQuest(BaseUrl+Link)
+    stock = BeautifulSoup(Detail.text,'html.parser')
+
+    FilterDetail = stock.find("div",{"id":"bodyContent"})
+    published = stock.find("strong",{"class":"published"})
+    context = ""
+    for i in FilterDetail.find_all('p'):
+        if i.find_parent(id = ['commentrequest','social_bookmarks']):
+            continue
+        context += i.text if i.getText().strip() else ""
+    print (context)
 
 if __name__ == '__main__':
-    scrape('https://news.ycombinator.com/newest')
+    spider()
