2639afab28aeaec0da9a9380a23d0b31339064cf
K-n15 <theepapon7055@gmail.com>
2026-01-01 13:46:54 +0700

    Finally finished the scraper to return latest news Changed the api call method to REST instead of SDK Modified the code into class for building object

--- Seabed.py
+++ Seabed.py
@@ -1,55 +1,99 @@
-import requests, re, os
-from google import genai
+import requests, os
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 
-load_dotenv()
-Summarizer = genai.Client(api_key = os.getenv("AI_KEY"))
+class Lobster:
+    def __init__(self):
+        self.BaseUrl = 'https://en.wikinews.org'
+        self.LatestTitle = ""
+        self.LastestRelease = ""
+        self.LatestSums  = ""
+        self.FullUrl = ""
+        self.Scavenging()
 
-def spiderQuest(url):
-    headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
-    response = requests.get(url,headers=headers,timeout=10)
-    if response.status_code == 408 :
-        return "REQUEST_TIMEOUT"
-    elif  response.status_code == 504:
-        return "GATEWAY_TIMEOUT"
-    return response
+    def getLatestNew(self):
+        return self.LatestSums,self.FullUrl
+    
+    def Scavenge(self):
+        x = self.Scavenging()
+        return "OK" if x else x
 
-def Conclude(context):
-    text = "Summarize this in one paragraph:\n" + context
-    try:
-        result = Summarizer.models.generate_content(
-            model="gemini-2.5-flash",
-            contents=text
-        )
-    except Exception as e:
-        return "Error : " + str(e)
-    else:
-        return result.text
+    def LobsterQuest(self,url):
+        headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
+        #2 retry chances
+        exc,err = None,None
+        for i in range(2):
+            try:
+                response = requests.get(url,headers=headers,timeout=10)
+                status = response.status_code
+                if status == 200:
+                    return response
+                else:
+                    err = response
+                    continue
+            except Exception as e:
+                exc = e
+                continue
+        return exc if err else err
 
-def spider():
-    BaseUrl = 'https://en.wikinews.org'
+    def Conclude(self,context):
+        load_dotenv()
+        try:
+            API_KEY = os.environ.get("GEMINI_API_KEY") 
+            MODEL = "gemini-2.5-flash"
+            url = f"https://generativelanguage.googleapis.com/v1beta/models/{MODEL}:generateContent"
+            headers = { 
+                "Content-Type": "application/json",
+                "x-goog-api-key": API_KEY,
+                }
+            text = "Summarize this in one paragraph:\n" + context
+            data = {
+                "contents": [{
+                    "parts": [{"text": text}]
+                }]
+            }
+            response = requests.post(url, headers=headers, json=data, timeout=10)
+            if response.status_code == 200:
+                result = response.json()
+                text = result['candidates'][0]['content']['parts'][0]['text']
+                print("--- SUCCESS ---")
+                return text
+            else:
+                print(f"--- API Error: {response.status_code} ---")
+                print(response.text)
+                return response.text
 
-    latestNew = spiderQuest(BaseUrl+'/wiki/Main_Page')
-    soupBase = BeautifulSoup(latestNew.text,'html.parser')
+        except Exception as e:
+            print(f"--- Network Failed: {e} ---")
+            return "Error : " + str(e)
 
-    FilterTitle = soupBase.find("div",{"id":"MainPage_latest_news_text","class":"latest_news_text"})
+    def Scavenging(self):
+        latestNew = self.LobsterQuest(self.BaseUrl+'/wiki/Main_Page')
+        if latestNew.status_code != 200:
+            return "Error ["+str(latestNew.status_code)+"] : "+latestNew.text
+        soupBase = BeautifulSoup(latestNew.text,'html.parser')
 
-    FirstNews = FilterTitle.find('a')
-    Topic = FirstNews.text
-    Link = FirstNews['href']
+        FilterTitle = soupBase.find("div",{"id":"MainPage_latest_news_text","class":"latest_news_text"})
+        FirstNews = FilterTitle.find('a')
+        self.LatestTitle = FirstNews.text
+        self.FullUrl = FirstNews['href']
 
-    Detail = spiderQuest(BaseUrl+Link)
-    stock = BeautifulSoup(Detail.text,'html.parser')
+        Detail = self.LobsterQuest(self.BaseUrl+self.FullUrl)
+        if latestNew.status_code != 200:
+            return "Error ["+str(Detail.status_code)+"] : "+Detail.text
+        stock = BeautifulSoup(Detail.text,'html.parser')
 
-    FilterDetail = stock.find("div",{"id":"bodyContent"})
-    published = stock.find("strong",{"class":"published"})
-    context = ""
-    for i in FilterDetail.find_all('p'):
-        if i.find_parent(id = ['commentrequest','social_bookmarks']):
-            continue
-        context += i.text if i.getText().strip() else ""
-    Conclude(context)
+        FilterDetail = stock.find("div",{"id":"bodyContent"})
+        self.LastestRelease = stock.find("strong",{"class":"published"}).text
+        context = ""
+        for i in FilterDetail.find_all('p'):
+            if i.find_parent(id = ['commentrequest','social_bookmarks']):
+                continue
+            context += i.text if i.getText().strip() else ""
+        summarize = self.Conclude(context)
+        print("Success")
+        self.LatestSums  = self.LastestRelease + ' : ' + self.LatestTitle + '\n' + summarize
 
 if __name__ == '__main__':
-    spider()
+    seafood = Lobster()
+    print(seafood.getLatestNew())
